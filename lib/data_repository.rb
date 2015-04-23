f7228ecefe566f588201c6e01a43f3a8d747f303
Feng Jia <jia.feng@rea-group.com>
2015-04-23 23:35:07 +0800

    add cache for data fetching

--- lib/data_repository.rb
+++ lib/data_repository.rb
@@ -5,6 +5,9 @@ class DataRepository
 
   SOAP_REQUEST_XML = '<soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns:q0="http://ws.itcast.cn/" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"> <soapenv:Body> <q0:findBikeSites></q0:findBikeSites></soapenv:Body> </soapenv:Envelope>'
 
+  @@data = nil
+  @@cached_at = Time.now
+
   def self.search_by_term term
     data.select do |station|
       station["location"].include?(term) || station["sitename"].include?(term)
@@ -14,7 +17,14 @@ class DataRepository
   private
 
   def self.data
+    fetch if (@@data.nil? || Time.now - @@cached_at > 600)
+    @@data
+  end
+
+  def self.fetch
+    p 'Fetching...'
     response = HTTParty.post(WS_URL, body: SOAP_REQUEST_XML)
-    JSON.parse(response.body.scan(/.*\<ns1:out\>(.*)\<\/ns1:out\>.*/)[0][0])
+    @@data  = JSON.parse(response.body.scan(/.*\<ns1:out\>(.*)\<\/ns1:out\>.*/)[0][0])
+    @@cached_at = Time.now
   end
 end
